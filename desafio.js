5eeae4a9764f3d014e8c56c27c2cd28bc1a18e5c
Ozeias Santos <ozeiasbss@gmail.com>
2022-11-14 16:43:56 -0300

    firts commit

---
+++ desafio.js
@@ -0,0 +1,11 @@
+/* 
+* Crie 2 arquivos JavaScript.
+* O Primeiro, irá exportar uma função chamada getFlag()
+* Que receberá um argumento do tipo string
+* Essa função deverá buscar dentro do arrray process.argv a flag deseja, que é a String
+* E retornar o valor da flag
+* O segundo irá importar a função e passar a flag desejada.
+* Iremos rodar no terminal o segundo artquivo passado as flags --name e --greeting
+* Para que seja impresso no termina a saudação e o nome da pessoa.
+*/
+
